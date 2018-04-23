29e60466ece06577ea1ad96b0c06b0b435f22630
atharvas <atharva2@illinois.edu>
2018-04-22 19:33:33 -0500

    redid everything.

---
+++ app/src/main/java/com/example/spotify_playlist/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.spotify_playlist;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Commands for Full-Screen
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //Launch Activity Main Layout
+        this.setContentView(R.layout.activity_main);
+        onUpdate();
+
+
+
+
+    }
+
+    private void onUpdate() {
+        SeekBar getHourSeeker = (SeekBar)findViewById(R.id.hourBar);
+        SeekBar getMinuteSeeker = (SeekBar)findViewById(R.id.minuteBar);
+        final TextView putHourText = (TextView)findViewById(R.id.dynamic_playlistHRS);
+        final TextView putMinuteText = (TextView)findViewById(R.id.dynamic_playlistMIN);
+        putHourText.setText(String.valueOf(0));
+        putMinuteText.setText(String.valueOf(0));
+
+        getHourSeeker.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                putHourText.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        getMinuteSeeker.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                putMinuteText.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+    }
+}
