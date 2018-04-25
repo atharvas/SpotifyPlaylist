fb50cf823944df579fff59eb3089f52c477b4a47
atharvas <atharva2@illinois.edu>
2018-04-25 17:00:21 -0500

    Spotify SDK Integration Added.

--- app/src/main/java/com/example/spotify_playlist/MainActivity.java
+++ app/src/main/java/com/example/spotify_playlist/MainActivity.java
@@ -2,16 +2,37 @@ package com.example.spotify_playlist;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Vibrator;
-import android.util.AndroidException;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Config;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Error;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerEvent;
+import com.spotify.sdk.android.player.Spotify;
+import com.spotify.sdk.android.player.SpotifyPlayer;
+
+public class MainActivity extends Activity implements
+        SpotifyPlayer.NotificationCallback, ConnectionStateCallback
+{
+    // TODO: Replace with your client ID
+    private static final String CLIENT_ID = "4712c40461f848f58a65ccd92acba7d2";
+
+    // TODO: Replace with your redirect URI
+    private static final String REDIRECT_URI = "yourcustomprotocol://callback";
+
+    private Player mPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +43,63 @@ public class MainActivity extends Activity {
         //Launch Activity Main Layout
         this.setContentView(R.layout.activity_main);
         onUpdate();
-
-
-
-
     }
 
-    private void onUpdate() {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onPlaybackEvent(PlayerEvent playerEvent) {
+        Log.d("MainActivity", "Playback event received: " + playerEvent.name());
+        switch (playerEvent) {
+            // Handle event type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onPlaybackError(Error error) {
+        Log.d("MainActivity", "Playback error received: " + error.name());
+        switch (error) {
+            // Handle error type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d("MainActivity", "User logged in");
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Log.d("MainActivity", "User logged out");
+    }
+
+    @Override
+    public void onLoginFailed(Error var1) {
+        Log.d("MainActivity", "Login failed");
+    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d("MainActivity", "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("MainActivity", "Received connection message: " + message);
+    }
+        private void onUpdate() {
         SeekBar getHourSeeker = (SeekBar)findViewById(R.id.hourBar);
         SeekBar getMinuteSeeker = (SeekBar)findViewById(R.id.minuteBar);
         final TextView putHourText = (TextView)findViewById(R.id.dynamic_playlistHRS);
@@ -79,9 +150,42 @@ public class MainActivity extends Activity {
 
 
     public void executeUAuth(android.view.View playlistCreate) {
-        Toast.makeText(getApplicationContext(), "UAuth pressed", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "oAuth pressed", Toast.LENGTH_SHORT).show();
         Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         if (vibe != null)
             vibe.vibrate(40);
     }
 }
+
+//package com.example.spotify_playlist;
+//
+//import android.app.Activity;
+//import android.content.Context;
+//import android.os.Bundle;
+//import android.os.Vibrator;
+//import android.util.AndroidException;
+//import android.view.Window;
+//import android.view.WindowManager;
+//import android.widget.SeekBar;
+//import android.widget.TextView;
+//import android.widget.Toast;
+//
+//public class MainActivity extends Activity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        //Commands for Full-Screen
+//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        //Launch Activity Main Layout
+//        this.setContentView(R.layout.activity_main);
+//        onUpdate();
+//
+//
+//
+//
+//    }
+//
+
+//}
